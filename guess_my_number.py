b64ff2090cb0c8bab8f082e0be60460804f35d15
JohnVanNote <johnlvannote@gmail.com>
2017-09-18 16:46:26 -0400

    Made it not terrible

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,36 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # guess_my_number.py
 #
-# simple guess my number program
+# Simple guess my number program
 #
-# created by: john van note
-# created on 9/11/13
+# created by: John Van Note
 #
 
 import sys
 import random
 
 def main():
-  name = raw_input( "Enter your name: " )
+  """Main Program"""
+  name = raw_input("Enter your name: ")
   print "Hello " + name
-  max_range = raw_input( "Enter the maximum number you would like: " )
+  max_range = raw_input("Enter the maximum number you would like: ")
   if not isinstance( max_range, int ):
     print "error"
 
-  random_num = generate_random( max_range )
+  random_num = generate_random(max_range)
   print random_num
   user_correct = False
-  while( user_correct != True ):
-    user_guess = int( raw_input( "Take a guess: " ))
-    user_correct = judge( random_num, user_guess )
+  while(user_correct != True):
+    user_guess = int(raw_input("Take a guess: "))
+    user_correct = judge(random_num, user_guess)
   return 0
 
-def generate_random( max_range ):
-  return random.randrange( max_range );
+
+def generate_random(max_range):
+  """Generate's Random Number"""
+  return random.randrange(max_range)
 
 
-def judge( random_num, user_guess ):
+def judge(random_num, user_guess):
+  """Checks"""
   if random_num == user_guess:
     print "correct, you win"
     return True
--- makefile
+++ makefile
@@ -1,3 +1,8 @@
+# Makefile
+#
+# John Van Note
+#
+
 run : guess_my_number.py
 	python guess_my_number.py
 
